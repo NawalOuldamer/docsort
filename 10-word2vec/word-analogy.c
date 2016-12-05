89949339e0c81779992228b7f7478f23b43e548b
mdeboc <mdeboc@zoho.com>
2016-12-05 18:07:23 +0100

    update distance, compute acc, makefile, word-a, word2phr

--- 10-word2vec/word-analogy.c
+++ 10-word2vec/word-analogy.c
@@ -28,7 +28,6 @@ int main(int argc, char **argv) {
   char file_name[max_size], st[100][max_size];
   float dist, len, bestd[N], vec[max_size];
   long long words, size, a, b, c, d, cn, bi[100];
-  char ch;
   float *M;
   char *vocab;
   if (argc < 2) {
@@ -50,17 +49,33 @@ int main(int argc, char **argv) {
     return -1;
   }
   for (b = 0; b < words; b++) {
-    fscanf(f, "%s%c", &vocab[b * max_w], &ch);
-    for (a = 0; a < size; a++) fread(&M[a + b * size], sizeof(float), 1, f);
+    a = 0;
+    while (1) {
+      vocab[b * max_w + a] = fgetc(f);
+      if (feof(f) || (vocab[b * max_w + a] == ' ')) break;
+      if ((a < max_w) && (vocab[b * max_w + a] != '\n')) a++;
+    }
+    vocab[b * max_w + a] = 0;
+    for (a = 0; a < size; a++) {
+      fread(&M[a + b * size], sizeof(float), 1, f);
+    }
     len = 0;
-    for (a = 0; a < size; a++) len += M[a + b * size] * M[a + b * size];
+    for (a = 0; a < size; a++) {
+      len += M[a + b * size] * M[a + b * size];
+    }
     len = sqrt(len);
-    for (a = 0; a < size; a++) M[a + b * size] /= len;
+    for (a = 0; a < size; a++) {
+      M[a + b * size] /= len;
+    }
   }
   fclose(f);
   while (1) {
-    for (a = 0; a < N; a++) bestd[a] = 0;
-    for (a = 0; a < N; a++) bestw[a][0] = 0;
+    for (a = 0; a < N; a++) {
+      bestd[a] = 0;
+    }
+    for (a = 0; a < N; a++) {
+      bestw[a][0] = 0;
+    }
     printf("Enter three words (EXIT to break): ");
     a = 0;
     while (1) {
@@ -93,8 +108,12 @@ int main(int argc, char **argv) {
       continue;
     }
     for (a = 0; a < cn; a++) {
-      for (b = 0; b < words; b++) if (!strcmp(&vocab[b * max_w], st[a])) break;
-      if (b == words) b = 0;
+      for (b = 0; b < words; b++) {
+        if (!strcmp(&vocab[b * max_w], st[a])) break;
+      }
+      if (b == words) {
+        b = 0;
+      }
       bi[a] = b;
       printf("\nWord: %s  Position in vocabulary: %lld\n", st[a], bi[a]);
       if (b == 0) {
@@ -104,22 +123,36 @@ int main(int argc, char **argv) {
     }
     if (b == 0) continue;
     printf("\n                                              Word              Distance\n------------------------------------------------------------------------\n");
-    for (a = 0; a < size; a++) vec[a] = M[a + bi[1] * size] - M[a + bi[0] * size] + M[a + bi[2] * size];
+    for (a = 0; a < size; a++) {
+      vec[a] = M[a + bi[1] * size] - M[a + bi[0] * size] + M[a + bi[2] * size];
+    }
     len = 0;
-    for (a = 0; a < size; a++) len += vec[a] * vec[a];
+    for (a = 0; a < size; a++) {
+      len += vec[a] * vec[a];
+    }
     len = sqrt(len);
-    for (a = 0; a < size; a++) vec[a] /= len;
-    for (a = 0; a < N; a++) bestd[a] = 0;
-    for (a = 0; a < N; a++) bestw[a][0] = 0;
+    for (a = 0; a < size; a++) {
+      vec[a] /= len;
+    }
+    for (a = 0; a < N; a++) {
+      bestd[a] = 0;
+    }
+    for (a = 0; a < N; a++) {
+      bestw[a][0] = 0;
+    }
     for (c = 0; c < words; c++) {
       if (c == bi[0]) continue;
       if (c == bi[1]) continue;
       if (c == bi[2]) continue;
       a = 0;
-      for (b = 0; b < cn; b++) if (bi[b] == c) a = 1;
+      for (b = 0; b < cn; b++) {
+        if (bi[b] == c) a = 1;
+      }
       if (a == 1) continue;
       dist = 0;
-      for (a = 0; a < size; a++) dist += vec[a] * M[a + c * size];
+      for (a = 0; a < size; a++) {
+        dist += vec[a] * M[a + c * size];
+      }
       for (a = 0; a < N; a++) {
         if (dist > bestd[a]) {
           for (d = N - 1; d > a; d--) {
@@ -132,7 +165,9 @@ int main(int argc, char **argv) {
         }
       }
     }
-    for (a = 0; a < N; a++) printf("%50s\t\t%f\n", bestw[a], bestd[a]);
+    for (a = 0; a < N; a++) {
+      printf("%50s\t\t%f\n", bestw[a], bestd[a]);
+    }
   }
   return 0;
 }
