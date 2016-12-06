d26f12cb4f91c513e20cf4474f972f2846509b65
mdeboc <mdeboc@zoho.com>
2016-12-06 17:10:11 +0100

    real preprocessing stage 1: from .json with mongo

---
+++ preprocessing/text-cleaning-mongo.py
@@ -0,0 +1,32 @@
+
+# -*- coding: utf8 -*-
+
+# USAGE:
+# python text-cleaning-mongo.py
+
+# preliminaries
+from pymongo import MongoClient
+import pandas as pd
+import time
+data_root = "/Users/matthieu/Documents/data_word2vec/"
+
+n_lim = 1800000
+
+# grab data from database and convert to pandas dataframe
+client = MongoClient()
+db = client.scrapping  # access target database
+collection = db.job_offers  # access target collection within the target database
+
+start = time.time()
+
+data = pd.DataFrame(list(collection.find().limit(n_lim)))  # 500000 each row is one document; the raw text of the document should be in the 'text_data' column
+data = data['jobDescription']
+
+
+# http://docs.python.org/howto/unicode.html    p.agent_info = u' '.join((agent_contact, agent_telno)).encode('utf-8').strip()
+
+with open(data_root+"out1m8.txt", 'w') as wrf:
+    for ind in range(n_lim): wrf.write(data[ind].encode('utf-8')+"\n")
+
+end = time.time()
+print end - start
